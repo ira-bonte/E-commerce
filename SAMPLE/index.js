2f88231e19803604616befb280cd944b74f17bb9
Iradukunda Bonte <irabonte123@gmail.com>
2023-08-09 22:56:48 -0700

    other updates

---
+++ SAMPLE/index.js
@@ -0,0 +1,11 @@
+function navigateToAnotherPage() {
+    // Change the location to the desired URL
+    window.location.href = 'knife1.html';
+    window.location.href= 'knife2.html';
+    window.location.href= 'knife3.html';
+    window.location.href= 'knife4.html';
+    window.location.href= 'knife5.html';
+    window.location.href= 'knife6.html';
+    window.location.href= 'knife7.html';
+    window.location.href= 'knife8.html';
+}
